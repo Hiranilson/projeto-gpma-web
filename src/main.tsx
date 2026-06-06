942a79d3505d451d8a56ad83b8783c3d43a8f56d
José Victor <jose.victor.nascimento.017@ufrn.edu.br>
2026-06-05 23:10:17 -0300

    chore: configure biome and tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,37 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-import "./index.css"
-import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import './index.css'
+import { ThemeProvider } from '@/components/theme-provider.tsx'
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-)
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+function InnerApp() {
+  return <RouterProvider router={router} />
+}
+
+// biome-ignore lint/style/noNonNullAssertion: We check if the element is empty before rendering, so it's safe to assert that it exists.
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = createRoot(rootElement)
+
+  root.render(
+    <StrictMode>
+      <ThemeProvider>
+        <InnerApp />
+      </ThemeProvider>
+    </StrictMode>
+  )
+}
